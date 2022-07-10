34b9f830f84810172e13fb4b8f4a4827d627d548
William <william.viana2018@hotmail.com>
2022-07-10 16:11:12 -0300

    components-props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Nome from './components/Nome';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Bem Vindo</h1>
+  return(
+    <div>
+      <h1>Componente App</h1>
+      <Nome aluno="Arthur Dias" idade={30}/>
+      <br/>
+      <Nome aluno="Jose Silva" idade={28}/>
     </div>
   );
 }
---
+++ src/components/Nome.js
@@ -0,0 +1,7 @@
+function Nome({ aluno, idade }) {
+    return (
+      <span>Bem Vindo: {aluno} - Idade: {idade} anos</span>
+    )
+  }
+
+  export default Nome;
